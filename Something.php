c45efd42b8e8eabe066026d99abcaee669fb8f10
Meago <65340448+Meago@users.noreply.github.com>
2020-05-14 13:10:36 +0300

    Update and rename Something.html to Something.php

---
+++ Something.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+ניסיונות:  <input type="text" id="tries" name="tries"><br><br>
+הצלחות: <input type="text" id="suc" name="suc"><br><br>
+<canvas id="graph" width="600" height="600"></canvas>
+</body>
+</html>
